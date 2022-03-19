3c292f25e9ad849b878fe1474cf933a0d9a9311a
aliperbol <alicititat@gmail.com>
2022-03-19 19:16:51 +0100

    primera pagina

---
+++ app/src/main/java/com/example/tinderus/IniciarSesion.kt
@@ -0,0 +1,12 @@
+package com.example.tinderus
+
+import androidx.appcompat.app.AppCompatActivity
+
+import android.os.Bundle
+
+class IniciarSesion : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.iniciar_sesion)
+    }
+}
