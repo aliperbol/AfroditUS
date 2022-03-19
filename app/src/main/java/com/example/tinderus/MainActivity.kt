3f19211f1ba3333be489a289184c9d69cee1682f
aliperbol <alicititat@gmail.com>
2022-03-19 19:15:43 +0100

    primera pagina

--- app/src/main/java/com/example/tinderus/MainActivity.kt
+++ app/src/main/java/com/example/tinderus/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.tinderus
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonClick = findViewById<Button>(R.id.button)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, IniciarSesion::class.java)
+            startActivity(intent)
+        }
+        val buttonClick2 = findViewById<Button>(R.id.button2)
+        buttonClick2.setOnClickListener {
+            val intent = Intent(this, Registro::class.java)
+            startActivity(intent)
+        }
     }
+
 }
