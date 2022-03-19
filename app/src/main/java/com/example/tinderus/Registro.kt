83724a98c04915e77a4e66f9a64cbcc60bfce0f5
almgonlop <almu.gon.12@gmail.com>
2022-03-19 20:56:28 +0100

    Página de registro y poca más

--- app/src/main/java/com/example/tinderus/Registro.kt
+++ app/src/main/java/com/example/tinderus/Registro.kt
@@ -1,12 +1,20 @@
 package com.example.tinderus
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 
 import android.os.Bundle
+import android.widget.Button
 
 class Registro : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.registro)
+
+        val buttonClick = findViewById<Button>(R.id.BotonEmpezar)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, Primer_perfil::class.java)
+            startActivity(intent)
+        }
     }
 }
