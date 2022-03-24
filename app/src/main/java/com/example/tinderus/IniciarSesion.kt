a329b70bfa0902e44aa6c8cfde0f9d562e7d8988
mangonreg <mangonreg@alum.us.es>
2022-03-24 17:57:14 +0100

    icono app

--- app/src/main/java/com/example/tinderus/IniciarSesion.kt
+++ app/src/main/java/com/example/tinderus/IniciarSesion.kt
@@ -6,7 +6,10 @@ import android.os.Bundle
 
 class IniciarSesion : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.iniciar_sesion)
+
+
     }
 }
