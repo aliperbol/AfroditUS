4e24337f01f614fe7bed4b89663e74522367cc71
almgonlop <almu.gon.12@gmail.com>
2022-03-24 17:56:58 +0100

    Primer Perfil

--- app/src/main/java/com/example/tinderus/Primer_perfil.kt
+++ app/src/main/java/com/example/tinderus/Primer_perfil.kt
@@ -4,13 +4,29 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 
+enum class ProviderType{
+    BASIC
+}
 class Primer_perfil:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.primer_perfil)
+        setContentView(com.example.tinderus.R.layout.primer_perfil);
 
+        val spinner= findViewById<Spinner>(R.id.seleccionPref)
 
+        val lista= resources.getStringArray(R.array.preferencias)
+
+        val adaptador = ArrayAdapter(this, android.R.layout.simple_spinner_item, lista)
+        spinner.adapter = adaptador
+
+        val buttonClick = findViewById<Button>(R.id.button)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, Intereses::class.java)
+            startActivity(intent)
+        }
     }
 }
