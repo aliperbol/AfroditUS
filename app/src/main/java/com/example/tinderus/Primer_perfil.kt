971eba376223595b0601bcc5cdb24a6e2551429e
mangonreg <mangonreg@alum.us.es>
2022-04-04 16:52:36 +0200

    imagen perfil

--- app/src/main/java/com/example/tinderus/Primer_perfil.kt
+++ app/src/main/java/com/example/tinderus/Primer_perfil.kt
@@ -3,15 +3,12 @@ package com.example.tinderus
 import android.app.Activity
 import android.content.Intent
 import android.graphics.drawable.BitmapDrawable
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 
 import android.os.Bundle
 import android.provider.MediaStore
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.Spinner
-import android.widget.TextView
+import android.net.Uri
+import android.widget.*
 
 
 class Primer_perfil:AppCompatActivity() {
@@ -22,25 +19,32 @@ class Primer_perfil:AppCompatActivity() {
 
         setContentView(R.layout.primer_perfil);
 
+        //Recogemos los datos enviados en la actividad anterior
         val bundle = intent.extras
+        //Tomamos el nombre de usuario del registro
         val username = bundle?.getString("username")
 
         findViewById<TextView>(R.id.nombreUsuario).text = username
 
+        //Seleccionar la preferencia sexual
         val spinner = findViewById<Spinner>(R.id.seleccionPref)
 
+        //Lista de preferencias
         val lista = resources.getStringArray(R.array.preferencias)
 
+        //Objeto completo preferencias
         val adaptador = ArrayAdapter(this, android.R.layout.simple_spinner_item, lista)
         spinner.adapter = adaptador
 
+        //Onclick para finalizar y conducir a intereses
         val buttonClick = findViewById<Button>(R.id.botonHaciaIntereses)
         buttonClick.setOnClickListener {
             val intent = Intent(this, Intereses::class.java)
             startActivity(intent)
         }
 
-        findViewById<Button>(R.id.selectImage).setOnClickListener{
+        //Colocamos un onclick sobre la imagen para porder subir el archivo
+        findViewById<ImageView>(R.id.selectImage).setOnClickListener{
             val intent = Intent(Intent.ACTION_PICK)
             intent.type = "image/*"
             startActivityForResult(intent, 0)
@@ -48,16 +52,19 @@ class Primer_perfil:AppCompatActivity() {
 
     }
 
+    //Declaramos la variable que almacenará la foto
     var fotoSeleccionadaURL: Uri? = null
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         if(requestCode == 0 && resultCode == Activity.RESULT_OK && data != null){
             fotoSeleccionadaURL= data.data
-            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, fotoSeleccionadaURL)
-            val bitmapDrawable = BitmapDrawable(bitmap)
+            //val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, fotoSeleccionadaURL)
+            //val bitmapDrawable = BitmapDrawable(bitmap)
 
-            findViewById<Button>(R.id.selectImage).setBackgroundDrawable(bitmapDrawable)
+            //findViewById<Button>(R.id.selectImage).setBackgroundDrawable(bitmapDrawable)
+            val imagen  = findViewById<ImageView>(R.id.selectImage)
+            imagen.setImageURI(fotoSeleccionadaURL)
         }
 
 
