b3b597a4fbf10409e007dcfcb0612bdb4674bb07
aliperbol <alicititat@gmail.com>
2022-04-06 20:51:35 +0200

    subir imagen

--- app/src/main/java/com/example/tinderus/ListaChats.kt
+++ app/src/main/java/com/example/tinderus/ListaChats.kt
@@ -8,7 +8,6 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tinderus.Chat
 import com.example.tinderus.ChatAdapter
 import com.example.tinderus.R
-import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import java.util.*
 
