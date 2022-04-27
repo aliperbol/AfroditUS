47029e1789988efbde1487d9fe2be43c54803fc8
aliperbol <alicititat@gmail.com>
2022-04-27 20:14:07 +0200

    perfiles de usuarios ajenos terminado

---
+++ app/src/main/java/com/example/tinderus/PerfilAjeno.kt
@@ -0,0 +1,77 @@
+package com.example.tinderus
+
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import java.io.File
+
+class PerfilAjeno : AppCompatActivity(){
+    private val auth = Firebase.auth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.perfil_ajeno)
+
+        val bundle = intent.extras
+        //Tomamos el nombre de usuario del registro
+        val nombre = bundle?.getString("nombre") ?: ""
+        val descripcion = bundle?.getString("descripcion")?: ""
+        val intereses = bundle?.getString("intereses")?: ""
+        val fotoPerfil = bundle?.getString("fotoPerfil")?: ""
+
+        val nombreVista = findViewById<TextView>(R.id.nombreajeno)
+        val descripcionVista = findViewById<TextView>(R.id.descripcionajena)
+        val fotoPerfilVista = findViewById<ImageView>(R.id.imagenajena)
+
+        val localfile = File.createTempFile("tempImage", "jpg")
+
+        FirebaseStorage.getInstance().getReferenceFromUrl(fotoPerfil).getFile(localfile).addOnSuccessListener {
+            val bitmap = BitmapFactory.decodeFile(localfile.absolutePath)
+            fotoPerfilVista.setImageBitmap(bitmap)
+        }
+        nombreVista.text = nombre
+        descripcionVista.text =descripcion
+
+
+
+
+
+
+        //Al iniciar la app, estableceremos que los botones de la barra de menu principal tengan
+        //un onclick preestablecido
+
+        val chatButton = findViewById<ImageView>(R.id.imgChats)
+        chatButton.setOnClickListener{
+
+            val intent = Intent(this,ListOfChats::class.java)
+            intent.putExtra("usuario", auth.currentUser?.uid)
+            startActivity(intent)
+
+
+        }
+
+        val initButton = findViewById<ImageView>(R.id.imgInicio)
+        initButton.setOnClickListener{
+            val intent = Intent(this, MenuPrincipal::class.java)
+            startActivity(intent)
+        }
+
+        val perfilButton = findViewById<ImageView>(R.id.imgPerfil)
+        perfilButton.setOnClickListener{
+            val intent = Intent(this, Perfil_propio::class.java)
+            startActivity(intent)
+        }
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/tinderus/Usuario.kt
@@ -0,0 +1,15 @@
+package com.example.tinderus
+
+import java.util.*
+import kotlin.collections.ArrayList
+
+data class Usuario (
+    var nombre: String = "",
+    var edad: String = "s",
+    var descripcion: String = "",
+    var preferencia: String = "",
+    var genero: String = "",
+    var uid: String ="",
+    var fotoPerfil: String ="",
+    var intereses: ArrayList<String> = ArrayList()
+)
