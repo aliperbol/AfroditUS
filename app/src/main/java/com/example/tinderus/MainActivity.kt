f6e5c24a58439433495ababae2adac46f53ea7b5
almgonlop <almu.gon.12@gmail.com>
2022-04-28 17:47:19 +0200

    edad en perfil externo

--- app/src/main/java/com/example/tinderus/MainActivity.kt
+++ app/src/main/java/com/example/tinderus/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,7 +23,7 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-
+        supportActionBar?.hide()
     }
 
 }
