214024021e5d33d520a2372c4e409f112384d6db
mangonreg <mangonreg@alum.us.es>
2022-04-28 17:41:26 +0200

    perfil propio

--- app/src/main/java/com/example/tinderus/Perfil_propio.kt
+++ app/src/main/java/com/example/tinderus/Perfil_propio.kt
@@ -4,11 +4,14 @@ import android.content.Intent
 import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -25,6 +28,7 @@ class Perfil_propio  : AppCompatActivity() {
     private lateinit var  imagenPerfil:ImageView
     private lateinit var descripcionPerfil:TextView
     private lateinit var interesesPerfil:TextView
+    private lateinit var nombrePerfil:TextView
 
     /////////////////////
     //    Funciones    //
@@ -35,9 +39,10 @@ class Perfil_propio  : AppCompatActivity() {
         setContentView(R.layout.perfilpropio)
 
         //Inicializamos variables
-        imagenPerfil = findViewById<ImageView>(R.id.imagenPerfil)
-        descripcionPerfil = findViewById<TextView>(R.id.descripcionPerfil)
-        interesesPerfil = findViewById<TextView>(R.id.interesesPerfil)
+        imagenPerfil = findViewById<ImageView>(R.id.imagenajena)
+        descripcionPerfil = findViewById<TextView>(R.id.descripcionajena)
+        interesesPerfil = findViewById<TextView>(R.id.interesesajenos)
+        nombrePerfil = findViewById<TextView>(R.id.nombreajeno)
 
         //Al iniciar la app, estableceremos que los botones de la barra de menu principal tengan
         //un onclick preestablecido
@@ -73,34 +78,79 @@ class Perfil_propio  : AppCompatActivity() {
             startActivity(intent)
         }
 
-        // Una vez se implementan los listener en los botones, debemos incluir la imagen y la descripción
-        // del usuario que ha iniciado sesión
 
-        //Obtenemos la imagen de storage de firebase y la ponemos en el perfil del usuario
 
-        val localfile = File.createTempFile("tempImage", "jpj")
-        var urlimagen = Firebase.database.getReference("Usuarios").child(auth.currentUser.toString()).child("fotoPerfilURL")
-        FirebaseStorage.getInstance().getReferenceFromUrl(urlimagen.toString()).getFile(localfile).addOnSuccessListener {
-            val bitmap = BitmapFactory.decodeFile(localfile.absolutePath)
-            imagenPerfil.setImageBitmap(bitmap)
+        fun tomaDatosUsuario(usuario:DatabaseReference){
+            // Una vez se implementan los listener en los botones, debemos incluir la imagen y la descripción
+            // del usuario que ha iniciado sesión
+
+            val valueEventListener: ValueEventListener = object : ValueEventListener {
+
+
+                //Cada vez que cambie algún dato, se modificará directamente en la pagina
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                    //Inicializamos las variables con los datos que tomamos del usuario
+                    val nombre = dataSnapshot.child("nombre").getValue(String::class.java) ?:""
+                    val edad = dataSnapshot.child("edad").getValue(String::class.java)?:""
+                    val descripcion = dataSnapshot.child("descripcion").getValue(String::class.java)?:""
+                    val preferencia = dataSnapshot.child("preferencia").getValue(String::class.java)?:""
+                    val genero = dataSnapshot.child("genero").getValue(String::class.java)?:""
+                    val uid = dataSnapshot.child("uid").getValue(String::class.java)?:""
+                    val imagen = dataSnapshot.child("fotoPerfilURL").getValue(String::class.java)?:""
+                    var intereses = ArrayList<String>()
+                    Log.d("FragmentActivity", "Error: $imagen")
+
+
+                    for (i in dataSnapshot.child("intereses").children){
+                        intereses.add(i.getValue(String::class.java)?:"")
+                    }
+
+
+                    //De esta forma conseguimos accedemos a nuestro recycledView para mostrar usuarios como grid
+
+
+                    //Obtenemos la imagen de storage de firebase y la ponemos en el perfil del usuario
+
+                    val localfile = File.createTempFile("tempImage", "jpj")
+                    FirebaseStorage.getInstance().getReferenceFromUrl(imagen).getFile(localfile).addOnSuccessListener {
+                        val bitmap = BitmapFactory.decodeFile(localfile.absolutePath)
+                        imagenPerfil.setImageBitmap(bitmap)
+                    }
+
+                    //Obtenemos la descripción y los gustos del usuario y la incluimos en el perfil
+                    //Colocamos la descripción del usuario
+                    descripcionPerfil.text = descripcion
+
+                    //Recorremos sus intereses y los incluimos a modo de texto
+
+                    var interesesEnTexto: String = ""
+                    for(interes in intereses){
+                        if (intereses.indexOf(interes) == (intereses.size -1)){
+                            interesesEnTexto += interes
+                        }
+                        else{
+                            interesesEnTexto += interes+ ", "
+                        }
+                    }
+                    interesesPerfil.text = interesesEnTexto
+
+                    //Cambiamos el nombre del perfil
+                    nombrePerfil.text = nombre
         }
 
-        //Obtenemos la descripción y los gustos del usuario y la incluimos en el perfil
-        var usuarioActual = Firebase.database.getReference("Usuarios").child(auth.currentUser?.uid.toString())
-            //Colocamos la descripción del usuario
-            descripcionPerfil.text = usuarioActual.child("descripcion").toString()
-
-            //Recorremos sus intereses y los incluimos a modo de texto
-            var intereses:List<String> = usuarioActual.child("intereses") as List<String>
-            var interesesEnTexto: String = ""
-            for(interes in intereses){
-                if (intereses.indexOf(interes) == (intereses.size -1)){
-                    interesesEnTexto.plus(interes)
-                }
-                else{
-                    interesesEnTexto.plus(interes+ ", ")
-                }
-            }
-            interesesPerfil.text = interesesEnTexto
+        override fun onCancelled(databaseError: DatabaseError) {
+            Log.d("FragmentActivity", "Error: ${databaseError.message}")
         }
+
     }
+            usuario.addListenerForSingleValueEvent(valueEventListener)
+    }
+        //Llamamos a la función
+
+        val usuario = FirebaseDatabase.getInstance().getReference("Usuarios").child(auth.currentUser?.uid.toString())
+        Log.d("FragmentActivity", "Usuario escogido: $usuario")
+        tomaDatosUsuario(usuario)
+    }
+
+}
